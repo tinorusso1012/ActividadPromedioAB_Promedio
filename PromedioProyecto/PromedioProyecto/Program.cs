7a4722bc7fcbce401e39e1557eb4f176503508d1
tinorusso1012 <valentinorusso2002@gmail.com>
2023-06-08 20:18:26 -0300

    Actividad #1 Valentino Russo Barbagelata

--- PromedioProyecto/PromedioProyecto/Program.cs
+++ PromedioProyecto/PromedioProyecto/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PromedioProyecto;
 
 namespace PromedioProyecto
 {
@@ -10,6 +7,53 @@ namespace PromedioProyecto
     {
         static void Main(string[] args)
         {
+
+            Promedio miPromedio = new Promedio();
+
+            Console.WriteLine("Bienvenido al ejercicio Promedio :D\n");
+
+            Console.WriteLine("Elija una de las siguientes Opciones: \n");
+            bool verificar = true;
+            while (verificar)
+            {
+                Console.WriteLine("1- Ingresar un numero \n");
+
+                Console.WriteLine("2- Mostrar Promedio \n");
+
+                Console.WriteLine("3- Terminar Programa \n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Ingresa un numero: \n");
+                        double numero = Convert.ToDouble(Console.ReadLine());
+                        miPromedio.AcumularNumero(numero);
+                        Console.WriteLine("Numero Acumulado con exito \n");
+                        Console.WriteLine("Presiona una tecla para volver al Menu");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        double promedio = miPromedio.CalcularPromedio();
+                        Console.WriteLine("El Promedio Actual es de: {0} \n", promedio);
+                        Console.WriteLine("Presiona una tecla para volver al Menu");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3: verificar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ingreso un Valor Incorrecto sera redirigido al Menu Principal: \n");
+                        Console.WriteLine("Presiona una tecla para continuar");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+                
+            }
+
         }
     }
 }
--- PromedioProyecto/PromedioProyecto/Promedio.cs
+++ PromedioProyecto/PromedioProyecto/Promedio.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PromedioProyecto
 {
     class Promedio
     {
+        int cont;
+        double acumulador;
+        public Promedio()
+        {
+            this.cont = 0;
+            this.acumulador = 0;
+        }
+
+        public void AcumularNumero(double num)
+        {
+            
+            this.acumulador += num;
+            cont++;
+            
+            
+        }
+
+        public double CalcularPromedio()
+        {
+            double resultado = 0;
+            if (cont > 0)
+            {
+                resultado = this.acumulador / this.cont;
+            }
+            return resultado;
+        }
     }
 }
